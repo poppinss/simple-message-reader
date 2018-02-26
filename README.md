31a9a425a06102f8e71c5376eaf94203ae2e881a
Harminder <virk.officials@gmail.com>
2018-02-26 08:13:19 +0530

    feat(*): first working commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Simple message reader
+> Convert `ArrayBuffer` or `Blob` to string (Browser only)
+
+The websocket or XHR requests now can return data as a [Blob](https://developer.mozilla.org/en-US/docs/Web/API/Blob) or [ArrayBuffer](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/ArrayBuffer). Using this package, you can conver them back to string.
+
+```bash
+npm i simple-message-reader
+```
+
+```js
+const simpleMessageReader = require('simple-message-reader')
+const ws = Websocket('...')
+
+ws.on('message', (message) => {
+  simpleMessageReader(message, (error, output) => {
+    if (error) {
+      console.log(error)
+      return
+    }
+
+    console.log(output)
+  })
+})
+```
---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict'
+
+/**
+ * websocket-message-reader
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
+/**
+ * Converts array buffer to string
+ *
+ * @method bufferToString
+ *
+ * @param  {ArrayBuffer}       buff
+ * @param  {Function}          cb
+ *
+ * @return {void}
+ */
+function bufferToString (buff, cb) {
+  cb(null, String.fromCharCode.apply(null, new Uint8Array(buff)))
+}
+
+/**
+ * Reads blob to a string
+ *
+ * @method blobToString
+ *
+ * @param  {Blob}       input
+ * @param  {Function}   cb
+ *
+ * @return {void}
+ */
+function blobToString (input, cb) {
+  const reader = new window.FileReader()
+
+  reader.onload = (event) => cb(null, event.target.result)
+  reader.onerror = (event) => cb(event)
+  reader.onabort = (event) => cb(event)
+
+  reader.readAsText(input)
+}
+
+/**
+ * Convert ArrayBuffer or Blob to a string, if
+ * value is unknow, will be returned as it is
+ *
+ * @param  {Mixed}    input
+ * @param  {Function} cb
+ *
+ * @return {Mixed}
+ */
+module.exports = function (input, cb) {
+  if (input instanceof window.Blob === true) {
+    return blobToString(input, cb)
+  }
+
+  if (ArrayBuffer.isView(input)) {
+    return bufferToString(input, cb)
+  }
+
+  cb(null, input)
+}
