ec6dfe299532601a3a91188c7a583061b197c9a3
Harminder <virk.officials@gmail.com>
2018-02-27 17:14:38 +0530

    feat(*): change API
    Now API returns specific functions over a sandbox function

--- README.md
+++ README.md
@@ -9,18 +9,129 @@ The websocket or XHR requests now can return data as a [Blob](https://developer.
 npm i simple-message-reader
 ```
 
+Pull from CDN
+
+```html
+<script src="https://unpkg.com/simple-message-reader"></script>
+```
+
 ```js
-const simpleMessageReader = require('simple-message-reader')
-const ws = Websocket('...')
+import { isBlob, isArrayBuffer, parseBlob, parseArrayBuffer } from 'simple-message-reader'
 
-ws.on('message', (message) => {
-  simpleMessageReader(message, (error, output) => {
-    if (error) {
-      console.log(error)
-      return
-    }
+// from CDN
+const simpleMessageReader = window.simpleMessageReader
+```
 
-    console.log(output)
-  })
-})
+## API
+
+## Constants
+
+<dl>
+<dt><a href="#acceptedTypesMap">acceptedTypesMap</a> : <code>Object</code></dt>
+<dd><p>List of output formats accepted when parsing
+blobs</p>
+</dd>
+</dl>
+
+## Functions
+
+<dl>
+<dt><a href="#isBlob">isBlob(input)</a> ⇒ <code>Boolean</code></dt>
+<dd><p>Returns a boolean telling if value is blob or
+not</p>
+</dd>
+<dt><a href="#isArrayBuffer">isArrayBuffer(input)</a> ⇒ <code>Boolean</code></dt>
+<dd><p>Returns a boolean telling if value is any of
+the ArrayBuffer view or not</p>
+</dd>
+<dt><a href="#parseBlob">parseBlob(input, cb, [outputFormat])</a> ⇒ <code>void</code></dt>
+<dd><p>Parses the blob to any of the accepted outputFormat types.</p>
+<p>Callback will be called with error, if <code>outputFormat</code> is invalid
+or <code>input</code> is not a blob.</p>
+</dd>
+<dt><a href="#parseArrayBuffer">parseArrayBuffer(input)</a> ⇒ <code>String</code></dt>
+<dd><p>Parses the arrayBuffer to a string</p>
+</dd>
+</dl>
+
+<a name="acceptedTypesMap"></a>
+
+## acceptedTypesMap : <code>Object</code>
+List of output formats accepted when parsing
+blobs
+
+**Kind**: global constant  
+**Attribute**: acceptedTypesMap  
+**Example**  
+```js
+['arrayBuffer', 'binaryString', 'dataURL', 'text']
+```
+<a name="isBlob"></a>
+
+## isBlob(input) ⇒ <code>Boolean</code>
+Returns a boolean telling if value is blob or
+not
+
+**Kind**: global function  
+
+| Param | Type |
+| --- | --- |
+| input | <code>Blob</code> | 
+
+**Example**  
+```js
+isBlob(new Blob())
+```
+<a name="isArrayBuffer"></a>
+
+## isArrayBuffer(input) ⇒ <code>Boolean</code>
+Returns a boolean telling if value is any of
+the ArrayBuffer view or not
+
+**Kind**: global function  
+
+| Param | Type |
+| --- | --- |
+| input | <code>ArrayBufferView</code> | 
+
+**Example**  
+```js
+isArrayBuffer(new Uint8Array())
+```
+<a name="parseBlob"></a>
+
+## parseBlob(input, cb, [outputFormat]) ⇒ <code>void</code>
+Parses the blob to any of the accepted outputFormat types.
+
+Callback will be called with error, if `outputFormat` is invalid
+or `input` is not a blob.
+
+**Kind**: global function  
+
+| Param | Type | Default |
+| --- | --- | --- |
+| input | <code>Blob</code> |  | 
+| cb | <code>function</code> |  | 
+| [outputFormat] | <code>String</code> | <code>&#x27;arrayBuffer&#x27;</code> | 
+
+**Example**  
+```js
+parseBlob(new Blob(), function (error, output) {
+
+}, 'arrayBuffer')
+```
+<a name="parseArrayBuffer"></a>
+
+## parseArrayBuffer(input) ⇒ <code>String</code>
+Parses the arrayBuffer to a string
+
+**Kind**: global function  
+
+| Param | Type |
+| --- | --- |
+| input | <code>ArrayBufferView</code> | 
+
+**Example**  
+```js
+parseArrayBuffer(new Uint8Array([72, 101, 108, 108, 111, 32, 33])
 ```
--- bundle.js
+++ bundle.js
@@ -1,1 +1,97 @@
-!function(e,n){"object"==typeof exports&&"object"==typeof module?module.exports=n():"function"==typeof define&&define.amd?define([],n):"object"==typeof exports?exports.simpleMessageReader=n():e.simpleMessageReader=n()}(window,function(){return function(e){var n={};function r(t){if(n[t])return n[t].exports;var o=n[t]={i:t,l:!1,exports:{}};return e[t].call(o.exports,o,o.exports,r),o.l=!0,o.exports}return r.m=e,r.c=n,r.d=function(e,n,t){r.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:t})},r.r=function(e){Object.defineProperty(e,"__esModule",{value:!0})},r.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(n,"a",n),n},r.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},r.p="",r(r.s=0)}([function(e,n,r){"use strict";e.exports=function(e,n){return e instanceof window.Blob==!0?function(e,n){var r=new window.FileReader;r.onload=function(e){return n(null,e.target.result)},r.onerror=function(e){return n(e)},r.onabort=function(e){return n(e)},r.readAsText(e)}(e,n):ArrayBuffer.isView(e)?function(e,n){n(null,String.fromCharCode.apply(null,new Uint8Array(e)))}(e,n):void n(null,e)}}])});
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["simpleMessageReader"] = factory();
+	else
+		root["simpleMessageReader"] = factory();
+})(window, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\n/**\n * websocket-message-reader\n *\n * (c) Harminder Virk <virk@adonisjs.com>\n *\n * For the full copyright and license information, please view the LICENSE\n * file that was distributed with this source code.\n*/\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\nexports.isBlob = isBlob;\nexports.isArrayBuffer = isArrayBuffer;\nexports.parseBlob = parseBlob;\nexports.parseArrayBuffer = parseArrayBuffer;\nvar acceptedTypesMap = {\n  'arrayBuffer': 'readAsArrayBuffer',\n  'binaryString': 'readAsBinaryString',\n  'dataURL': 'readAsDataURL',\n  'text': 'readAsText'\n};\n\nfunction isBlob(input) {\n  return input instanceof window.Blob === true;\n}\n\nfunction isArrayBuffer(input) {\n  return ArrayBuffer.isView(input);\n}\n\nfunction parseBlob(input, cb) {\n  var outputFormat = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'arrayBuffer';\n\n  var acceptedTypes = Object.keys(acceptedTypesMap);\n\n  if (acceptedTypes.indexOf(outputFormat) !== -1) {\n    cb(new Error(outputFormat + ' is not supported. Only ' + acceptedTypes.join(',') + ' are allowed'));\n    return;\n  }\n\n  if (!isBlob(input)) {\n    cb(new Error('Input must be blob'));\n    return;\n  }\n\n  var reader = new window.FileReader();\n  reader.onload = function (event) {\n    return cb(null, event.target.result);\n  };\n  reader.onerror = function (event) {\n    return cb(event);\n  };\n  reader.onabort = function (event) {\n    return cb(event);\n  };\n  reader[acceptedTypesMap[outputFormat]](input);\n}\n\nfunction parseArrayBuffer(input) {\n  return String.fromCharCode.apply(null, new Uint8Array(input));\n}\n\n//# sourceURL=webpack://simpleMessageReader/./index.js?");
+
+/***/ })
+
+/******/ });
+});
